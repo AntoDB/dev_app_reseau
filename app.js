d559bd71dc3de767c55a042762e9125c5dafbbbd
AntoDB <antonindebreuck@gmail.com>
2024-05-01 19:56:23 +0200

    Revert "Test ajout langage - 26 avril 2024" (i18n Package) + Add good multilingual version
    This reverts commit (only part link to i18n package) :
    14db27cef80cfa135c6ca40e50f4c2f26a59b07f.
    + Add good multilingual solution

--- app.js
+++ app.js
@@ -67,51 +67,29 @@ async function getDataFromMongoDB() {
 /* ==================== Web server ==================== */  
 const express = require('express');
 const app = express();
+const path = require('path');
 
+// Configuration du moteur de modèle EJS
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 app.use('/fonts', express.static(__dirname + '/fonts'));
 app.use('/css', express.static(__dirname + '/css'));
-app.use('/locales', express.static(__dirname + '/locales'));
 
-/* ==================== Language treatment ==================== */
-const i18n = require('i18n');
-
-// Configuration de i18n
-i18n.configure({
-    locales: ['en', 'fr'], // Langues prises en charge
-    defaultLocale: 'en', // Langue par défaut
-    directory: __dirname + '/locales', // Répertoire contenant les fichiers de traduction
-    objectNotation: true, // Utilisation de la notation d'objet
-    queryParameter: 'lang', // Paramètre de requête pour spécifier la langue dans l'URL
+// Middleware pour le multilinguisme
+app.use((req, res, next) => {
+    const lang = req.url.split('/')[1];
+    if (lang === 'fr') {
+        res.locals.lang = require('./locales/fr.json'); // Répertoire avec le fichier de traduction fr.json
+    } else {
+        res.locals.lang = require('./locales/en.json'); // Répertoire avec le fichier de traduction en.json
+    }
+    next();
 });
 
-// Middleware pour la gestion des langues
-app.use(i18n.init);
-/* ==================== END Language treatment ==================== */
-
-// Créer un routeur pour les pages statiques
-const staticRouter = express.Router();
-
-staticRouter.get('/', async (req, res) => {
+// Routes
+app.get('/', (req, res) => {
     try {
-        const lang = req.params.lang || 'en'; // Récupérer la langue à partir de req.params.lang ou utiliser 'en' par défaut pour la page d'accueil
-        
-        const dataFromSTIB = await fetchDataFromSTIBAPI();
-        await insertDataIntoMongoDB(dataFromSTIB);
-        
-        // Récupérer les données de MongoDB
-        const dataFromMongoDB = await getDataFromMongoDB();
-
-        res.render('index', {
-            title: res.__('title'),
-            subtitle: res.__('subtitle'),
-            message: res.__('message'),
-            button_1: res.__('button_1'),
-            button_2: res.__('button_2'),
-            lang: lang, // Transmettre la langue à votre template EJS
-            mongoData: dataFromMongoDB // Transmettre les données à votre template EJS
-        });
+        res.render('index', { lang: res.locals.lang });
     } catch (error) {
         res.status(500).send('Une erreur est survenue.');
         console.error('Erreur :', error);
@@ -119,49 +97,28 @@ staticRouter.get('/', async (req, res) => {
     }
 });
 
-/*staticRouter.get('/:lang/', async (req, res) => {
+app.get('/:lang', (req, res) => {
     try {
-        const lang = req.params.lang; // Récupérer la langue à partir de req.params.lang
-
-        const dataFromSTIB = await fetchDataFromSTIBAPI();
-        await insertDataIntoMongoDB(dataFromSTIB);
-        
-        // Récupérer les données de MongoDB
-        const dataFromMongoDB = await getDataFromMongoDB();
-
-        res.render('index', {
-            title: res.__('title'),
-            subtitle: res.__('subtitle'),
-            message: res.__('message'),
-            button_1: res.__('button_1'),
-            button_2: res.__('button_2'),
-            lang: lang, // Transmettre la langue à votre template EJS
-            mongoData: dataFromMongoDB // Transmettre les données à votre template EJS
-        });
+        res.render('index', { lang: res.locals.lang });
     } catch (error) {
         res.status(500).send('Une erreur est survenue.');
         console.error('Erreur :', error);
         throw error;
     }
-});*/
+});
 
-staticRouter.get('/vehicle_positions', async (req, res) => {
+app.get('/:lang/vehicle_positions', async (req, res) => {
     try {
         //const lang = req.params.lang; // Récupérer la langue à partir de req.params.lang
-        const lang = 'en'
-
         const dataFromSTIB = await fetchDataFromSTIBAPI();
         await insertDataIntoMongoDB(dataFromSTIB);
         
         // Récupérer les données de MongoDB
         const dataFromMongoDB = await getDataFromMongoDB();
 
-        res.render('vehicle_positions', { 
-            title: res.__('vehicle_positions_title'),
-            subtitle: res.__('vehicle_positions_subtitle'),
-            description: res.__('vehicle_positions_description'), 
-            lang: lang, // Transmettre la langue à votre template EJS
-            mongoData: dataFromMongoDB // Transmettre les données à votre template EJS
+        res.render('vehicle_positions', {
+            lang: res.locals.lang, // Transmettre la langue au template EJS
+            mongoData: dataFromMongoDB // Transmettre les données au template EJS
         });
     } catch (error) {
         res.status(500).send('Une erreur est survenue.');
@@ -170,9 +127,7 @@ staticRouter.get('/vehicle_positions', async (req, res) => {
     }
 });
 
-// Utiliser le routeur avec la langue choisie pour les pages statiques
-app.use('/', staticRouter);
-
+// Démarrage du serveur
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Le serveur écoute sur le port ${port}`);
