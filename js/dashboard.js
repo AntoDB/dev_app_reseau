340e3b5ac76d38e5c477b7f511da22cbc9828d2f
AntoDB <antonindebreuck@gmail.com>
2024-05-10 23:20:24 +0200

    Add modals to edit or add value + Change navbar
    - Add modals to edit or add value with selected data
    - Modals CCS
    - Change navbar to go direct to a dashboard page (et not to the login systematically)
    - Remove useless data from all vehicle_positions

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,40 @@
 /* This script is for the admin option on the dashboard */
 
+// Fonction pour récupérer les données par leur ID via la REST API
+async function getDataByIdFromAPI(id) {
+    try {
+        const response = await fetch(`/api/data/${id}`);
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            throw new Error(`Erreur lors de la récupération des données avec l'identifiant ${id}`);
+        }
+    } catch (error) {
+        console.error('Une erreur est survenue lors de la récupération des données depuis l\'API :', error);
+        throw error;
+    }
+}
+
+// Fonction pour afficher le modal de modification de valeur via l'API REST
+async function displayEditModal(index,id) {
+    try {
+        // Récupérer les données correspondant à l'ID via l'API REST
+        const data = await getDataByIdFromAPI(id);
+        // Mettre à jour les champs du modal avec les données récupérées
+        document.getElementById('index').value = index;
+        document.getElementById('edit_id').value = data._id;
+        document.getElementById('edit_line').value = data.lineid;
+        document.getElementById('edit_direction').value = data.directionId;
+        document.getElementById('edit_distance').value = data.distanceFromPoint;
+        document.getElementById('edit_point').value = data.pointId;
+        // Afficher le modal
+        $('#editDataModal').modal('show');
+    } catch (error) {
+        console.error('Une erreur est survenue lors de l\'affichage du modal de modification de valeur :', error);
+        // Gérer l'erreur
+    }
+}
 
 // Fonction pour supprimer les données dans MongoDB
 async function deleteThisDataLine(id) {
